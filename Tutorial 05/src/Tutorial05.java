f3d8755858e7441460a363403078d01643b44786
Hasal Fernando <witty.leo98@gmail.com>
2019-05-10 11:54:57 +0530

    Tutorial 05 added

---
+++ Tutorial 05/src/Tutorial05.java
@@ -0,0 +1,23 @@
+public class Tutorial05 {
+
+    public static void main(String[] args){
+
+        boolean raining = false;
+        int a = 5;
+        int b = 10;
+
+        // equals               ==
+        // greater than         >
+        // less than            <
+        // greater than/equals  >=
+        // less than/equals     <=
+        // not equals           !=
+
+        if(a != b){
+            System.out.println("a is not equals to b");
+        }
+        else{
+            System.out.println("a is equals to b");
+        }
+    }
+}
